43734ef20d126fd8097d48a6a5e72b3060643f77
Tan Nguyen <tan.nqd@gmail.com>
2021-03-02 00:23:12 -0800

    fixed script

--- hardware/scripts/build_project.tcl
+++ hardware/scripts/build_project.tcl
@@ -14,7 +14,7 @@ set_property board_part www.digilentinc.com:pynq-z1:part0:1.0 [current_project]
 source $sources_file
 
 # Add constraint file
-add_files -fileset constrs_1 -norecurse constrs/pynq-z1.xdc
+add_files -fileset constrs_1 -norecurse constr/pynq-z1.xdc
 
 update_compile_order -fileset sources_1
 
