c6556b831789865569c4b63e36bee87b1bdd7166
Tan Nguyen <tan.nqd@gmail.com>
2021-03-03 08:51:00 -0800

    [EECS151] fixed bugs; added multi-port memory module for regfile

--- hardware/src/EECS151.v
+++ hardware/src/EECS151.v
@@ -423,9 +423,8 @@ module SYNC_RAM_DP_WBE(q0, d0, addr0, en0, wbe0, q1, d1, addr1, en1, wbe1, clk);
       for (i = 0; i < 4; i = i+1) begin
         if (wbe0[i])
           mem[addr0][i*8 +: 8] <= d0[i*8 +: 8];
-        end
-        read_data0_reg <= mem[addr0];
       end
+      read_data0_reg <= mem[addr0];
     end
   end
 
@@ -435,8 +434,7 @@ module SYNC_RAM_DP_WBE(q0, d0, addr0, en0, wbe0, q1, d1, addr1, en1, wbe1, clk);
         if (wbe1[i])
           mem[addr1][i*8 +: 8] <= d1[i*8 +: 8];
         end
-        read_data1_reg <= mem[addr1];
-      end
+      read_data1_reg <= mem[addr1];
     end
   end
 
@@ -444,4 +442,50 @@ module SYNC_RAM_DP_WBE(q0, d0, addr0, en0, wbe0, q1, d1, addr1, en1, wbe1, clk);
   assign q1 = read_data1_reg;
 
 endmodule // SYNC_RAM_DP_WBE
+
+// Multi-port RAM with two asynchronous-read ports, one synchronous-write port
+module ASYNC_RAM_1W2R(d0, addr0, we0, q1, addr1, q2, addr2, clk);
+  parameter DWIDTH = 8;  // Data width
+  parameter AWIDTH = 8;  // Address width
+  parameter DEPTH = 256; // Memory depth
+  parameter MIF_HEX = "";
+  parameter MIF_BIN = "";
+  input clk;
+
+  input [DWIDTH-1:0] d0;    // Data input
+  input [AWIDTH-1:0] addr0; // Address input
+  input              we0;   // Write enable
+
+  input [AWIDTH-1:0] addr1; // Address input
+  output [DWIDTH-1:0] q1;
+
+  input [AWIDTH-1:0] addr2; // Address input
+  output [DWIDTH-1:0] q2;
+
+  (* ram_style = "distributed" *) reg [DWIDTH-1:0] mem [DEPTH-1:0];
+
+  integer i;
+  initial begin
+    if (MIF_HEX != "") begin
+      $readmemh(MIF_HEX, mem);
+    end
+    else if (MIF_BIN != "") begin
+      $readmemb(MIF_BIN, mem);
+    end
+    else begin
+      for (i = 0; i < DEPTH; i = i + 1) begin
+        mem[i] = 0;
+      end
+    end
+  end
+
+  always @(posedge clk) begin
+    if (we0)
+      mem[addr0] <= d0;
+  end
+
+  assign q1 = mem[addr1];
+  assign q2 = mem[addr2];
+
+endmodule // ASYNC_RAM_1W2R
 
