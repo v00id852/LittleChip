7ca5b5c24ebd15ed2599fcb04ff1ca04a6a552bf
Tan Nguyen <tan.nqd@gmail.com>
2021-04-05 08:53:15 -0700

    [chkpt3 WIP] added software code to verify AXI read/write with dma controller removed conv2D stuff

--- software/151_library/memory_map.h
+++ software/151_library/memory_map.h
@@ -9,11 +9,12 @@
 #define SWITCHES (*((volatile uint32_t*)0x80000028) & 0x03)
 #define LED_CONTROL (*((volatile uint32_t*)0x80000030))
 
-#define CONV2D_START      (*((volatile uint32_t*) 0x80000040))
-#define CONV2D_IDLE       (*((volatile uint32_t*) 0x80000044) & 0x02)
-#define CONV2D_DONE       (*((volatile uint32_t*) 0x80000044) & 0x01)
+#define DMA_START     (*((volatile uint32_t*) 0x80000030))
+#define DMA_IDLE      (*((volatile uint32_t*) 0x80000034) & 0x02)
+#define DMA_DONE      (*((volatile uint32_t*) 0x80000034) & 0x01)
+#define DMA_DIR       (*((volatile uint32_t*) 0x80000038))
+#define DMA_SRC_ADDR  (*((volatile uint32_t*) 0x8000003c))
+#define DMA_DST_ADDR  (*((volatile uint32_t*) 0x80000040))
+#define DMA_LEN       (*((volatile uint32_t*) 0x80000044))
 
-#define CONV2D_FM_DIM     (*((volatile uint32_t*) 0x80000048))
-#define CONV2D_WT_OFFSET  (*((volatile uint32_t*) 0x8000004c))
-#define CONV2D_IFM_OFFSET (*((volatile uint32_t*) 0x80000050))
-#define CONV2D_OFM_OFFSET (*((volatile uint32_t*) 0x80000054))
+// TODO(tan): define addresses for accelerator memory-mapped registers
---
+++ software/axi_test/axi_test.c
@@ -0,0 +1,70 @@
+#include "types.h"
+#include "ascii.h"
+#include "uart.h"
+#include "memory_map.h"
+
+#define BUF_LEN 128
+
+#define SIZE 8
+
+static int8_t array0[SIZE] = {0};
+static int8_t array1[SIZE] = {0};
+
+typedef void (*entry_t)(void);
+
+// This simple test loops data from DMem to DDR and back
+int main(int argc, char**argv) {
+  int8_t buffer[BUF_LEN];
+  int32_t len = SIZE;
+  int32_t i;
+
+  for (i = 0; i < len; i++) {
+    array1[i] = 2 * i;
+  }
+
+  // Copy data from array1 from DMem to DDR at address 0x40_0000
+  DMA_DIR = 1;
+  DMA_DST_ADDR = 0x400000;
+  DMA_SRC_ADDR = (uint32_t)array1 >> 2;
+  DMA_LEN = len;
+  DMA_START = 1;
+  while (!DMA_DONE);
+
+  // Copy data from DDR at address 0x40_0000 to array0 from DMem
+  DMA_DIR = 0;
+  DMA_SRC_ADDR = 0x400000;
+  DMA_DST_ADDR = (uint32_t)array1 >> 2;
+  DMA_LEN = 2;
+  DMA_START = 1;
+  while (!DMA_DONE);
+
+  uint32_t num_mismatches = 0;
+  // Make sure that the two arrays match!
+  for (i = 0; i < len; i++) {
+    uwrite_int8s("\r\n>>> At ");
+    uwrite_int8s(uint8_to_ascii_hex(i, buffer, BUF_LEN));
+    uwrite_int8s("\r\narray0 ");
+    uwrite_int8s(uint8_to_ascii_hex(array0[i], buffer, BUF_LEN));
+    uwrite_int8s("\r\narray1 ");
+    uwrite_int8s(uint8_to_ascii_hex(array1[i], buffer, BUF_LEN));
+
+    if (array0[i] != array1[i]) {
+      num_mismatches += 1;     
+    }
+  }
+
+  if (num_mismatches == 0)
+    uwrite_int8s("\r\nPassed!\r\n");
+  else {
+    uwrite_int8s("\r\nFailed! Num. mismatches ");
+    uwrite_int8s(uint8_to_ascii_hex(i, buffer, BUF_LEN));
+    uwrite_int8s("\r\n");
+  }
+
+  // go back to the bios - using this function causes a jr to the addr,
+  // the compiler "jals" otherwise and then cannot set PC[31:28]
+  uint32_t bios = ascii_hex_to_uint32("40000000");
+  entry_t start = (entry_t) (bios);
+  start();
+  return 0;
+}
