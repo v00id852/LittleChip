7e0df60d67a945ac4e04e0fbde8c83070b12af8f
Tan Nguyen <tan.nqd@gmail.com>
2021-04-13 12:42:25 -0700

    some final testings should work with all frequency values from 10ns to 20ns. not sure about anything beyond 10ns should work with Linux desktop, VM with Vivado, VM with HWServer + Remote lab machine

--- hardware/scripts/write_bitstream.tcl
+++ hardware/scripts/write_bitstream.tcl
@@ -29,10 +29,11 @@ if {[get_property PROGRESS [get_runs impl_1]] != "100%"} {
   error "ERROR: impl_1 failed"   
 } 
 
-write_hwdef -force  -file ${project_name}_proj/${project_name}_proj.sdk/hwdef.zip
-exec unzip ${project_name}_proj/${project_name}_proj.sdk/hwdef.zip -d ${project_name}_proj/${project_name}_proj.sdk
-
 # Backup bitstream
 file mkdir bitstream_files
 file copy -force [glob ${project_name}_proj/${project_name}_proj.runs/impl_1/*.bit] bitstream_files/${project_name}.bit
 
+if {${project_name} eq "z1top_axi"} {
+write_hwdef -force  -file ${project_name}_proj/${project_name}_proj.sdk/hwdef.zip
+exec unzip ${project_name}_proj/${project_name}_proj.sdk/hwdef.zip -d ${project_name}_proj/${project_name}_proj.sdk
+}
