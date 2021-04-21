3bd6c1f1ea034f607de25e7ab231827cf0d816aa
Tan Nguyen <tan.nqd@gmail.com>
2021-04-20 21:31:55 -0700

    fixed write_bitsream script to ensure Vivado exits gracefully

--- hardware/scripts/write_bitstream.tcl
+++ hardware/scripts/write_bitstream.tcl
@@ -1,16 +1,30 @@
 
 #set project_name "z1top"
 set project_name [lindex $argv 0]
+set target_clock [lindex $argv 1]
 
 set sources_file scripts/${project_name}.tcl
 
 if {![file exists $sources_file]} {
-    puts "Invalid project name!"
-    exit
+  puts "Invalid project name!"
+  exit
 }
 open_project ${project_name}_proj/${project_name}_proj.xpr
 
-set sources_file scripts/${project_name}.tcl
+if {${project_name} eq "z1top_axi"} {
+set f_mhz [expr int(1000 / ${target_clock})]
+set f_hz  [expr int(1000000000 / ${target_clock})]
+
+open_bd_design ${project_name}_proj/${project_name}_proj.srcs/sources_1/bd/z1top_axi_bd/z1top_axi_bd.bd
+set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ ${f_mhz} CONFIG.PCW_USE_M_AXI_GP0 {0} CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1}] [get_bd_cells processing_system7_0]
+set_property -dict [list CONFIG.CPU_CLOCK_FREQ ${f_hz}] [get_bd_cells z1top_axi_0]
+set ps_clk [get_property CONFIG.FREQ_HZ [get_bd_pin processing_system7_0/FCLK_CLK0]]
+set_property -dict [list CONFIG.FREQ_HZ ${ps_clk}] [get_bd_intf_pins z1top_axi_0/interface_aximm]
+save_bd_design
+update_compile_order -fileset sources_1
+set_property top z1top_axi_bd_wrapper [current_fileset]
+}
+
 update_compile_order -fileset sources_1
 
 reset_run synth_1
@@ -35,5 +49,5 @@ file copy -force [glob ${project_name}_proj/${project_name}_proj.runs/impl_1/*.b
 
 if {${project_name} eq "z1top_axi"} {
 write_hwdef -force  -file ${project_name}_proj/${project_name}_proj.sdk/hwdef.zip
-exec unzip ${project_name}_proj/${project_name}_proj.sdk/hwdef.zip -d ${project_name}_proj/${project_name}_proj.sdk
+exec unzip -o ${project_name}_proj/${project_name}_proj.sdk/hwdef.zip -d ${project_name}_proj/${project_name}_proj.sdk
 }
