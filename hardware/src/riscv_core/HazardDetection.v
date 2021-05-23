9eb09edbc45eef8d0f7df4ce1bbe23d507af5384
lei <zianglei@126.com>
2021-05-23 20:18:57 +0800

    New: HazardDetection unit can detect control hazard and output stall control signal

--- hardware/src/riscv_core/HazardDetection.v
+++ hardware/src/riscv_core/HazardDetection.v
@@ -2,8 +2,15 @@
 
 module HAZARD_DETECTION (
   input clk,
+  input rst,
   input [6:0] opcode,
+  input [4:0] if_id_rs1,
+  input [4:0] if_id_rs2,
+  input [4:0] id_ex_rd,
+
   input ctrl_pc_src,
+  output ctrl_pc_en,
+  output ctrl_imem_en,
   output ctrl_id_reg_flush,
   output ctrl_zero_sel
 );
@@ -18,8 +25,13 @@ module HAZARD_DETECTION (
     .q(ctrl_id_reg_flush)
   );
 
-  assign inst_flush_value = ctrl_pc_src;
   // NOP
-  assign ctrl_zero_sel = opcode == 6'b0;
+  assign ctrl_zero_sel = (opcode == 6'b0) || (!ctrl_pc_en);
+
+  assign ctrl_pc_en = !(id_ex_rd != 0 && (if_id_rs1 == id_ex_rd || if_id_rs2 == id_ex_rd));
+  assign ctrl_imem_en = rst || ctrl_pc_en;
+
+  // Only when rst is high or imem is enabled and the pc value is updated from calculated one
+  assign inst_flush_value = rst || (ctrl_imem_en & ctrl_pc_src);
 
 endmodule
