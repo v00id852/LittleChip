320c5bee2d589332f87385844c02f14f9db8270b
lei <zianglei@126.com>
2021-05-27 23:28:34 +0800

    Add more ports to MMIO

--- hardware/src/riscv_core/MMIO.v
+++ hardware/src/riscv_core/MMIO.v
@@ -3,31 +3,43 @@ module MMIO #(
   parameter DWIDTH = 32
 ) (
   input [AWIDTH - 1:0] addr_in,
-  input [DWIDTH - 1:0] cycle_counter_in,
-  input [DWIDTH - 1:0] inst_counter_in,
+  input [DWIDTH - 1:0] data_in,
   input we_in,
-  output counter_rst_out,
-  output [DWIDTH - 1:0] data_out
+  // Peripheral data in
+  input [DWIDTH - 1:0] data_uart_rx_in,
+  input [DWIDTH - 1:0] data_cycle_counter_in,
+  input [DWIDTH - 1:0] data_inst_counter_in,
+  // Peripheral data in
+  input ctrl_uart_tx_ready_in,
+  input ctrl_uart_rx_valid_in,
+
+  output [DWIDTH - 1:0] data_reg_out,
+  // Peripheral data out
+  output [DWIDTH - 1:0] data_uart_tx_out,
+  // Peripheral control out
+  output ctrl_uart_tx_valid_out,
+  output ctrl_uart_rx_ready_out,
+  output ctrl_counter_rst_out
 );
 
-  reg [DWIDTH - 1:0] data_out;
-  reg [DWIDTH - 1:0] counter_rst_out;
+  reg [DWIDTH - 1:0] data_reg_out;
+  reg ctrl_counter_rst_out;
 
   always @(*) begin
-    if (addr == {{(AWIDTH - 32){1'b0}}, 32'h80000010}) begin
-      data_out = cycle_counter_in;
+    if (addr == {{(AWIDTH - 32) {1'b0}}, 32'h80000010}) begin
+      data_reg_out = cycle_counter_in;
     end else begin
-      data_out = 32'b0;
+      data_reg_out = 32'b0;
     end
   end
 
   always @(*) begin
-    if ((addr == {{(AWIDTH - 32){1'b0}}, 32'h80000018}) && we) begin
-      counter_rst_out = 1'b1;
+    if ((addr == {{(AWIDTH - 32) {1'b0}}, 32'h80000018}) && we) begin
+      ctrl_counter_rst_out = 1'b1;
     end else begin
-      counter_rst_out = 1'b0;
+      ctrl_counter_rst_out = 1'b0;
     end
   end
-  
+
 
 endmodule
