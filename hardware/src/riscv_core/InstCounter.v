dceaae0ce02b1fdce8aeb9226c32ff151cdfa300
lei <zianglei@126.com>
2021-05-30 23:16:10 +0800

    New: create instruction counter

---
+++ hardware/src/riscv_core/InstCounter.v
@@ -0,0 +1,22 @@
+module INST_COUNTER #(
+  parameter DWIDTH = 32
+) (
+  input clk,
+  input rst,
+  input [6:0] opcode,
+  output [DWIDTH - 1:0] counter_out
+);
+
+  wire [DWIDTH - 1:0] counter_value, counter_next;
+  
+  REGISTER_R #(.N(DWIDTH), .INIT(0)) counter_reg (
+    .clk(clk),
+    .rst(rst),
+    .d (counter_next),
+    .q (counter_value)
+  );
+  
+  assign counter_out = counter_value;
+  assign counter_next = (opcode == 6'd0) ? counter_value : counter_value + 1;
+
+endmodule
